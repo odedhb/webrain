88ff4bb6f69e7082bdaa7d6ab8b6f806d9da357d
Oded Breiner <https://linkedin.com/in/odedhb>
2015-02-24 10:26:48 +0200

    Infra: regex pattern matching

--- appengine/we-brain/main.py
+++ appengine/we-brain/main.py
@@ -1,25 +1,58 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+from google.appengine.ext import ndb
 import webapp2
+import re
+
+
+class Item(ndb.Model):
+    url_template = ndb.StringProperty()
+    # url_computed = ndb.ComputedProperty()
+    query_template = ndb.StringProperty()
+    # excerpt = ndb.StringProperty()
+    image = ndb.StringProperty()
+    # clicks = ndb.IntegerProperty()
+    updated = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class Search(webapp2.RequestHandler):
+    def get(self):
+        query = self.request.get("q")
+
+        item = Item(url_template="https://www.facebook.com/search/str/mike/keywords_top")
+        item.query_template = "flights from (?P<source>.+) to (?P<destination>.+)"
+        item.image = "https://cdn2.vox-cdn.com/uploads/chorus_asset/file/3397376/tbf_014_gates_education_thumb_raw.0.jpg"
+        item.clicks = 3
+        all_items = [item]
+
+        matching_items = []
+
+        for item in all_items:
+            if re.match(item.query_template, query):
+                matching_items.append(item)
+
+        html = SEARCH_BOX
+        for item in matching_items:
+            html = html + "<br><a href='" + item.url_template + "'>" + item.query_template + "</a><br>"
+
+        html += ADD_LINK
+        self.response.write(html)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(SEARCH_BOX)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+SEARCH_BOX = """
+<form action="../search">
+    <input type="text" name="q">
+    <input type="submit" value="go">
+</form>
+        """
+
+ADD_LINK = """
+<br><a href=''>Add +</a>
+        """
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/search', Search)]
+                              , debug=True)
